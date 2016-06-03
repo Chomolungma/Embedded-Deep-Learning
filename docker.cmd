5c73168bf59c67db97722579f2d46562103f7bb1
Chong <ws.chong.sg@gmail.com>
2016-06-03 14:43:05 +0800

    create
    draft1

---
+++ docker.cmd
@@ -0,0 +1,14 @@
+Guide to use docker image file for c3d
+
+caffe
+
+reference
+docker installation
+  http://qiita.com/daxanya1/items/f04c7f75a6d2ecb92b23
+
+nvidia-docker
+https://hub.docker.com/r/skydjol/nvidia-docker/
+
+C3D
+https://github.com/facebook/C3D
+sudo apt-get --assume-yes install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler; sudo apt-get install --no-install-recommends libboost-all-dev; sudo apt-get --assume-yes install libatlas-base-dev; sudo apt-get --assume-yes install libgflags-dev libgoogle-glog-dev liblmdb-dev
